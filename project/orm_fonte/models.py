ff336ae7712adae10609697c3dca5dd707f590a6
othon <othonbrener123@gmail.com>
2024-07-16 21:52:01 -0300

    feat(project/orm_fonte): adiciona tabela para o banco de dados fonte

---
+++ project/orm_fonte/models.py
@@ -0,0 +1,31 @@
+from typing import Iterator
+from sqlalchemy import create_engine, ForeignKey, func
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column, relationship, Session, registry
+
+from project.settings import Settings
+
+
+db_fonte_registry = registry()
+
+engine_fonte = create_engine(
+    f'postgresql://{Settings().POSTGRES_FONTE_USER}:'
+    f'{Settings().POSTGRES_FONTE_PASSWORD}@localhost:'
+    f'5432/{Settings().POSTGRES_FONTE_DB}'
+)
+
+
+def get_session_fonte() -> Iterator[Session]:
+    with Session(engine_fonte) as session:
+        yield session
+
+
+@db_fonte_registry.mapped_as_dataclass
+class Data:
+    __tablename__ = 'data'
+
+    id: Mapped[int] = mapped_column(primary_key=True, init=False)
+    timestamp: Mapped[datetime]
+    wind_speed: Mapped[float]
+    power: Mapped[float]
+    ambient_temperature: Mapped[float]
