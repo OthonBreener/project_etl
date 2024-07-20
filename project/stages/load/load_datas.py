94066f06227c3e231fbd4a33bc29d6dc031dac5b
othon <othonbrener123@gmail.com>
2024-07-20 11:41:33 -0300

    feat(project/stages/load/load_datas.py): adiciona etapa para de carregamento dos dados no banco de dados alvo

---
+++ project/stages/load/load_datas.py
@@ -0,0 +1,94 @@
+import pandas as pd
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from project.orm_alvo.models import (
+    engine_alvo, Signal
+)
+
+
+class LoadDatas:
+    def __init__(self, datas: pd.DataFrame) -> None:
+        self.datas = datas
+
+    def load(self) -> None:
+        data_frame = self.datas
+
+        data_frame_wind_speed = data_frame["wind_speed"].copy()
+        self._save_dataframe_wind_speed(data_frame_wind_speed)
+
+        data_frame_power = data_frame["power"].copy()
+        self._save_dataframe_power(data_frame_power)
+
+    def _save_dataframe_wind_speed(
+        self, data_frame_wind_speed: pd.DataFrame
+    ) -> None:
+        try:
+            signal = self._get_signal_by_name('wind_speed')
+
+            data_frame_wind_speed['signal_id'] = signal.id
+
+            data_frame_wind_speed.to_sql(
+                "data",
+                engine_alvo,
+                if_exists="append",
+                index=True,
+                index_label="timestamp"
+            )
+        except Exception as exception:
+            raise self.LoadErrorSaveSignal(
+                f"Error on save dataframe wind speed: {exception}"
+            ) from exception
+
+    def _save_dataframe_power(
+        self, data_frame_power: pd.DataFrame
+    ) -> None:
+        try:
+            signal = self._get_signal_by_name('power')
+
+            data_frame_power['signal_id'] = signal.id
+
+            data_frame_power.to_sql(
+                "data",
+                engine_alvo,
+                if_exists="append",
+                index=True,
+                index_label="timestamp"
+            )
+        except Exception as exception:
+            raise self.LoadErrorSaveSignal(
+                f"Error on save dataframe power: {exception}"
+            ) from exception
+
+    def _save_signal(self, name: str) -> Signal:
+
+        try:
+            with Session(engine_alvo) as session:
+                signal = Signal(name=name)
+                session.add(signal)
+                session.commit()
+                session.refresh(signal)
+
+            return signal
+
+        except Exception as exception:
+            raise self.LoadErrorSaveSignal(
+                f"Error on save signal: {exception}"
+            ) from exception
+
+    def _get_signal_by_name(self, name: str) -> Signal:
+        with Session(engine_alvo) as session:
+            signal = session.scalar(
+                select(Signal).where(Signal.name == name)
+            )
+
+        if not signal:
+            signal = self._save_signal(name)
+
+        return signal
+
+    class LoadErrorSaveSignal(Exception):
+        pass
+
+    class LoadError(Exception):
+        pass
