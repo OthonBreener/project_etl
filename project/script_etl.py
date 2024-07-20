e95882e86b499db62aeaea9c13fd7449fb28726d
othon <othonbrener123@gmail.com>
2024-07-20 11:42:11 -0300

    refactor: add tratativa na etapa de transformação dos dados

--- project/script_etl.py
+++
@@ -1,4 +0,0 @@
-"""
-Pipeline para transferir dados do banco de dados fonte
-para o alvo.
-"""
--- project/stages/transform/transform_datas.py
+++ project/stages/transform/transform_datas.py
@@ -5,18 +5,26 @@ class TransformDatas:
     def __init__(self, datas: list[dict[str, float | str]]) -> None:
         self.datas = datas
 
-    def transform(self):
-        data_frame = pd.DataFrame(self.datas)
+    def transform(self) -> pd.DataFrame:
+        try:
+            data_frame = pd.DataFrame(self.datas)
 
-        data_frame.set_index("timestamp", inplace=True)
+            data_frame["timestamp"] = pd.to_datetime(data_frame["timestamp"])
 
-        data_frame["timestamp"] = pd.to_datetime(data_frame["timestamp"])
+            data_frame.set_index("timestamp", inplace=True)
 
-        aggregations = data_frame.resample("10min").agg(
-            {
-                "wind_speed": ["mean", "min", "max", "std"],
-                "power": ["mean", "min", "max", "std"],
-            }
-        )
+            aggregations = data_frame.resample("10min").agg(
+                {
+                    "wind_speed": ["mean", "min", "max", "std"],
+                    "power": ["mean", "min", "max", "std"],
+                }
+            )
+        except Exception as expection:
+            raise self.TransformError(
+                f"Error on transform datas: {expection}"
+            ) from expection
 
         return aggregations
+
+    class TransformError(Exception):
+        pass
