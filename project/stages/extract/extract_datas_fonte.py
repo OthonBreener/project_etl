b1d187ef1f65e305a62c9e2b1f4e7cb8e43bca72
othon <othonbrener123@gmail.com>
2024-07-20 11:38:29 -0300

    refactor(project/stages/extract/extract_datas_fonte.py): adiciona tratativa para caso de erro

--- project/stages/extract/extract_datas_fonte.py
+++ project/stages/extract/extract_datas_fonte.py
@@ -7,9 +7,20 @@ class ExtractDatasFonte:
     def __init__(self, date: datetime) -> None:
         self.date = date
 
-    def extract(self) -> dict[str, float | str]:
-        url = f"http://localhost:8000/date?date={self.date}"
+    def extract(self) -> list[dict[str, float | str]]:
+        try:
+            url = f"http://localhost:8000/date?date={self.date}"
 
-        datas = Client().get(url).json()
+            datas = Client().get(url)
 
-        return datas
+            assert datas.status_code == 200
+
+            return datas.json()
+
+        except Exception as expection:
+            raise self.ExtractError(
+                f"Error on extract datas from fonte: {expection}"
+            ) from expection
+
+    class ExtractError(Exception):
+        pass
