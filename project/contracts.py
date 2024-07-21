724317478401ba5ce57ff4275affbafe063b0c03
othon <othonbrener123@gmail.com>
2024-07-20 22:36:44 -0300

    feat(project/contracts.py): adiciona contratos para retorno dos estagios

---
+++ project/contracts.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from pandas import DataFrame
+
+
+@dataclass(frozen=True)
+class ContractExtract:
+    datas: list[dict[str, float | str]]
+
+
+@dataclass(frozen=True)
+class ContractTransform:
+    data_frame: DataFrame
