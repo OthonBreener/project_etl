ba828fcefa81242af22acc91d9ce9d99e5282513
othon <othonbrener123@gmail.com>
2024-07-21 10:28:16 -0300

    style: ruff

--- project/main.py
+++ project/main.py
@@ -1,9 +1,10 @@
 from datetime import datetime
+
 from loguru import logger
 
 if __name__ == "__main__":
-    from project.scripts.generate_datas import generate_datas
     from project.pipeline import Pipeline
+    from project.scripts.generate_datas import generate_datas
 
     logger.info("Generating datas for 2024-01-01 to 2024-01-10")
     generate_datas()
