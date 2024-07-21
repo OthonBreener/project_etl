268bb920bce9c3652dc3de4ee43859e5712709f7
othon <othonbrener123@gmail.com>
2024-07-21 03:44:47 -0300

    feat(project): adiciona arquivo main, que roda a função generate_datas

---
+++ project/main.py
@@ -0,0 +1,5 @@
+
+if __name__ == "__main__":
+    from project.scripts.generate_datas import generate_datas
+
+    generate_datas()
--- project/scripts/generate_datas.py
+++ project/scripts/generate_datas.py
@@ -3,13 +3,26 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 
-from project.orm_fonte.models import engine_fonte
+import sys
+from pathlib import Path
+
+# Adiciona o diretório raiz do projeto ao PYTHONPATH
+project_root = Path(__file__).resolve().parents[2]
+sys.path.append(str(project_root))
 
 
 def generate_datas(
-    interval: tuple[datetime, datetime],
-    engine=engine_fonte,
+    interval: tuple[datetime, datetime] = (
+        datetime(2024, 1, 1),
+        datetime(2024, 1, 10)
+    ),
+    engine=None,
 ):
+    from project.orm_fonte.models import engine_fonte
+
+    if not engine:
+        engine = engine_fonte
+
     interval_datas = pd.date_range(interval[0], interval[1], freq="1min")
 
     data_frame = pd.DataFrame(
