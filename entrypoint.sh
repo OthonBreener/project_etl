d91cc480ea62b1e9e985a26646919043310a9dc8
othon <othonbrener123@gmail.com>
2024-07-21 10:27:04 -0300

    fix: altera ordem da execução do arquivo main, o pipeline precisa que a api esteja em pé

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ cd project/orm_fonte && poetry run alembic upgrade head
 cd ../orm_alvo && poetry run alembic upgrade head
 cd /app
 
-# Cria os dados fakes no banco de dados alvo
-poetry run python -m project.main
-
 # Inicia a aplicação
 poetry run uvicorn --host 0.0.0.0 --port 8000 project.api:app
+
+# Cria os dados fakes no banco de dados alvo e executa o pipeline
+poetry run python -m project.main
